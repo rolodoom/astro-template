903159fce733cd3cd9dc77025bbc216240a3bb8d
RoLo <rolodoom@gmail.com>
2026-02-25 12:04:08 -0500

    :recycle: Refacto structure and add som dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,4 +9,5 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  integrations: [icon()],
 });
